897dcf2fb28a922213b8094dde8931ce9c8b1f46
FordeD <a.i.kusak@foxford.ru>
2024-05-16 18:44:40 +0300

    Add layerGroups

--- script.js
+++ script.js
@@ -2302,20 +2302,20 @@ const goupMarkerTypesKeys = Object.keys(groupMarkerTypes);
 // groupMarkerTypes.includes() -> groupMarkerTypeNames[type]
 for (const type of types) {
   let curentGroupType = '';
+
   for (const groupType of goupMarkerTypesKeys) {
     if (groupMarkerTypes[groupType].includes(type)) {
       curentGroupType = groupType;
     }
   }
   if (!iconGroups[curentGroupType]) {
-    iconGroups[groupMarkerTypeNames[curentGroupType]] = new L.FeatureGroup();
-    legendMarkers[groupMarkerTypeNames[curentGroupType]] =
-      iconGroups[groupMarkerTypeNames[curentGroupType]];
+    iconGroups[curentGroupType] = new L.featureGroup();
   }
 
   if (!iconSubGroups[type]) {
-    iconSubGroups[legendNames[type]] = new L.FeatureGroup();
-    legendMarkers[legendNames[type]] = iconSubGroups[legendNames[type]];
+    iconSubGroups[type] = new L.featureGroup.subGroup(
+      iconGroups[curentGroupType],
+    );
   }
 
   // legendMarkers[legendNames[type]] = iconGroups[type];
@@ -2345,12 +2345,35 @@ for (const type of types) {
         offset: [0, 15],
       });
     }
-    iconGroups[groupMarkerTypeNames[curentGroupType]].addLayer(marker);
-    iconSubGroups[legendNames[type]].addLayer(marker);
+    iconGroups[curentGroupType].addLayer(marker);
+    iconSubGroups[type].addLayer(marker);
     MARKERS.push(marker);
     
   }
 }
+
+const layerControl = L.control.layers(null, null, { collapsed: false });
+let prevGroupName = '';
+for (const type of types) {
+  for (const groupType of goupMarkerTypesKeys) {
+    if (groupMarkerTypes[groupType].includes(type)) {
+      curentGroupType = groupType;
+    }
+  }
+  if (prevGroupName !== curentGroupType) {
+    prevGroupName = curentGroupType;
+    layerControl.addOverlay(
+      iconGroups[groupMarkerTypeNames[curentGroupType]],
+      groupMarkerTypeNames[curentGroupType],
+    );
+  }
+  layerControl.addOverlay(iconSubGroups[type], legendNames[type]);
+}
+layerControl.addTo(map);
+
+// new L.Control.CustomButtons(null, Object.assign(iconGroups, iconSubGroups), {
+//   collapsed: false,
+// }).addTo(map);
 
 localStorage.setItem('hidedMarkers', JSON.stringify(HIDED_MARKERS));
 
@@ -2563,8 +2586,6 @@ L.Control.CustomButtons = L.Control.Layers.extend({
     });
   },
 });
-
-new L.Control.CustomButtons(null, Object.assign(iconGroups,iconSubGroups), { collapsed: false }).addTo(map);
 
 const legendPlace = document.querySelector('.leaflet-control-layers');
 const visibleMarkersPlace = document.querySelector('.add-button');
