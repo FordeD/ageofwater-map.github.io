9f120bd2b07e869fc1eeb9b2e355555a71174c8d
FordeD <a.i.kusak@foxford.ru>
2024-05-18 14:23:22 +0300

    Add marker

--- script.js
+++ script.js
@@ -438,6 +438,37 @@ const worldPoints = {
         [SHIPS.T1.SONJA],
       ),
     ],
+    [
+      -153.09375,
+      218.28125,
+      generateDescription(
+        'Корабли T1',
+        'https://forded.github.io/ageofwater-map.github.io/icons/trash.png',
+        `Два корабля ${SHIPS.T1.JARL.name}.`,
+        [
+          RESPURCES.SCRAP,
+          RESPURCES.FIBER,
+          RESPURCES.WOOD,
+          RESPURCES.PLASTIC,
+          RESPURCES.MECHANISM,
+          RESPURCES.PLATS,
+          RESPURCES.AMMO_DETAILS,
+          RESPURCES.BARREL,
+          RESPURCES.COPPER,
+          RESPURCES.STEEL,
+          RESPURCES.COPPER_PLATES,
+        ],
+        [
+          ITEMS.CONSTRUCTIONS.MUSOR_SHIELD.T11,
+          ITEMS.CONSTRUCTIONS.MUSOR_SHIELD_DVOIN.T1,
+          ITEMS.CONSTRUCTIONS.RUBKA_SCORPENA,
+          ITEMS.CONSTRUCTIONS.CISTERNA,
+        ],
+        null,
+        null,
+        [SHIPS.T1.JARL],
+      ),
+    ],
   ],
   rangers: [
     [
