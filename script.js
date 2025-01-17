0a780b58e12dfb84182122933e753e5b5404a632
FordeD <a.i.kusak@foxford.ru>
2025-01-17 12:30:34 +0300

    Correct save

--- script.js
+++ script.js
@@ -3735,6 +3735,47 @@ L.Control.CustomButtons = L.Control.Layers.extend({
   _removeMarker: function () {
     this.createButton('Скрыть', 'remove-button');
   },
+  _addItem: function (obj) {
+    var label = document.createElement('label'),
+      checked = this._map.hasLayer(obj.layer),
+      input;
+    
+    if (!showedMarkers.includes(findKeyByValue(legendNames, obj.name))) {
+      this._map.removeLayer(iconGroups[type]);
+      checked = false;
+    }
+
+    if (obj.overlay) {
+      input = document.createElement('input');
+      input.type = 'checkbox';
+      input.className = 'leaflet-control-layers-selector';
+      input.defaultChecked = checked;
+    } else {
+      input = this._createRadioElement('leaflet-base-layers_' + Util.stamp(this), checked);
+    }
+
+    this._layerControlInputs.push(input);
+    input.layerId = Util.stamp(obj.layer);
+
+    DomEvent.on(input, 'click', this._onInputClick, this);
+
+    var name = document.createElement('span');
+    name.innerHTML = ' ' + obj.name;
+
+    // Helps from preventing layer control flicker when checkboxes are disabled
+    // https://github.com/Leaflet/Leaflet/issues/2771
+    var holder = document.createElement('span');
+
+    label.appendChild(holder);
+    holder.appendChild(input);
+    holder.appendChild(name);
+
+    var container = obj.overlay ? this._overlaysList : this._baseLayersList;
+    container.appendChild(label);
+
+    this._checkDisabledLayers();
+    return label;
+  },
   _onInputClick: function () {
     var inputs = this._layerControlInputs,
       input,
